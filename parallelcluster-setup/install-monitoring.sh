300aeb807bd01ecac078357e591a8fcdba3b50d5
coderodyhpc <79646720+coderodyhpc@users.noreply.github.com>
2021-03-12 19:31:01 -0500

    Update install-monitoring.sh

--- parallelcluster-setup/install-monitoring.sh
+++ parallelcluster-setup/install-monitoring.sh
@@ -26,13 +26,17 @@ case "${cfn_cluster_user}" in
 	centos)
 		version=$(rpm --eval %{centos_ver})
 		case "${version}" in
+		echo ${version} >> /home/centos/idio.txt
 		8)
 			dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
 			dnf install docker-ce --nobest -y
 			systemctl enable --now docker
 			usermod -a -G docker $cfn_cluster_user
+			echo "middle docker" >> /home/centos/idio.txt
 			curl -L https://github.com/docker/compose/releases/download/1.28.5/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 			chmod +x /usr/local/bin/docker-compose
+			echo "end docker" >> /home/centos/idio.txt
+
 		;;
 		7)
 			yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
