33e24e17b2c9f5801911a0f63c015c677a2bf21f
coderodyhpc <79646720+coderodyhpc@users.noreply.github.com>
2021-04-14 12:43:41 -0400

    Update 1m-cost-metrics.sh

--- custom-metrics/1m-cost-metrics.sh
+++ custom-metrics/1m-cost-metrics.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-#
-#
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: MIT-0
-#
-#
 
 #source the AWS ParallelCluster profile
 . /etc/parallelcluster/cfnconfig
 
 export AWS_DEFAULT_REGION=$cfn_region
-aws_region_long_name=$(python /usr/local/bin/aws-region.py $cfn_region)
+aws_region_long_name=$(python3 /usr/local/bin/aws-region.py $cfn_region)
 
 monitoring_dir_name=$(echo ${cfn_postinstall_args}| cut -d ',' -f 2 )
 monitoring_home="/home/${cfn_cluster_user}/${monitoring_dir_name}"
